b2024a2d9cf7682ca37bc72bfee1802f503a26f6
Matthew LI <Matthew.Li2@mountsinai.org>
2018-03-19 09:24:58 -0400

    updates

--- heap/PrintMaxTopKInMatrix.java
+++
@@ -1,9 +0,0 @@
-package src.heap;
-
-/**
- * 有N个长度不一样的数组,所有数组中的元素都是从小到大有序 排列的,请从大到小打印这N个数组整体的前K个数
- */
-public class PrintMaxTopKInMatrix {
-
-    // Class 2 problem 9 todo
-}
--- leetcode/lc96_UniqueBST.java
+++
@@ -1,9 +0,0 @@
-package src.leetcode;
-
-/**
- * Created by zizhengli on 1/27/17.
- */
-public class lc96_UniqueBST {
-
-    //TODO
-}
