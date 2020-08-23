3405046a271ba44d6b1a130af1d2061769ab8765
Matthew LI <matthew.li@salesforce.com>
2020-08-23 14:57:02 -0400

    updates

--- src/sorting/QuickSort.java
+++ src/sorting/QuickSort.java
@@ -18,9 +18,9 @@ public class QuickSort {
         if(start >= end) {
             return;
         }
-        int pivot = partition(start, end, array);
-        quickSort(start, pivot - 1, array);
-        quickSort(pivot + 1, end, array);
+        int[] pivot = threeWayPartition(array, start, end);
+        quickSort(start, pivot[0] - 1, array);
+        quickSort(pivot[1] + 1, end, array);
     }
 
     private static int partition(int start, int end, int[] array) {
@@ -36,11 +36,37 @@ public class QuickSort {
         return indexToPlace;
     }
 
+    public static int[] threeWayPartition(int[] array, int low, int high) {
+        if(array == null || array.length == 0) {
+            return new int[] {-1, -1};
+        }
+
+        int pivot = high;
+        int index = low;
+        high--;
+
+        while(index <= high) {
+            if(array[index] > array[pivot]) {
+                Swap.swap(array, index, high--);
+            } else if(array[index] < array[pivot]) {
+                Swap.swap(array, index++, low++);
+            } else {
+                index++;
+            }
+        }
+        Swap.swap(array, index, pivot);
+        return new int[] {low, index};
+    }
+
     public static void main(String[] args) {
-        int[] A = {16, 3, 1, 8, 5, 9, 10, 11, 3, 11, 20, 54, 23, 7};
+        int[] A = {16, 3, 7, 8, 7, 9, 10, 11, 3, 7, 20, 7, 23, 7, 16, 3, 8, 21, 54, 10, 7};
         QuickSort.sort(A);
         for(int i = 0; i < A.length; i++) {
             System.out.print(A[i] + " ");
         }
+        System.out.println("==================");
+        /*A = new int[] {16, 3, 7, 8, 7, 9, 10, 11, 3, 7, 20, 7, 23, 7};
+        int[] res = threeWayPartition(A, 0, A.length - 1);
+        System.out.print(res[0] + " " + res[1]);*/
     }
 }
